890e845a599e343399a16bbfd4bc79cd51bf4159
Kevin Grogan <kevin.grogan@omni.sc>
2020-12-28 12:17:40 -0800

    Removed unused view

--- bromo/bromo/Views/MainView.swift
+++ bromo/bromo/Views/MainView.swift
@@ -15,27 +15,19 @@ struct MainView: View {
         TabView {
             WorkoutGeneratorView(config: config, fetcher: WorkoutFetcher(config: config))
                 .tabItem {
-                    Image(systemName: "bolt.fill")
-                    Text("Generate Workout")
+                    Image(systemName: "bolt.circle")
                 }
             LogView()
                 .tabItem {
                     Image(systemName: "square.and.pencil")
-                    Text("Log")
                 }
             ConfigurationView(config: config)
                 .tabItem {
                     Image(systemName: "slider.horizontal.3")
-                    Text("Configure")
-                }
-            FormInspectorView()
-                .tabItem {
-                    Image(systemName: "video.fill")
-                    Text("Inspect Form")
                 }
         }
         .font(.headline)
-        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        .preferredColorScheme(.dark)
     }
 }
 
